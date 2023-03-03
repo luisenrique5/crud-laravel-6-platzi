5d1067caf142acabe824ba960859c71a802f4abf
luisito <supelanosuarezluisenrique5@gmial.com>
2023-03-02 23:12:46 -0500

    proyecto finalizado

--- app/Http/Controllers/usercontroller.php
+++ app/Http/Controllers/usercontroller.php
@@ -19,11 +19,17 @@ class usercontroller extends Controller
     }
 
     public function store(Request $request)
-    {
+    {   
+        $request->validate([
+            'name'      =>['required'],
+            'email'     =>['required', 'email', 'unique:users'],
+            'password'  =>['required', 'min:8']
+        ]);
+
         User::create([
             'name'=> $request->name,
             'email'=> $request->email,
-            'password'=> $request->password,
+            'password'=> bcrypt($request->password),
         ]);
 
         return back();
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -12,39 +12,71 @@
         <div class="container">
           <div class="row">
             <div class="col-sm-8 mx-auto">
-              <table class="table">
-                <thead>
-                  <tr>
-                    <th>ID</th>
-                    <th>Nombre</th>
-                    <th>Email</th>
-                    <th>&nbsp;</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  @foreach ($users as $user)
+              <div class="card border-0 shadow">
+                <div class="card-body">
+                  @if ($errors->any())
+                      <div class="alert alert-danger">
+                        @foreach($errors->all() as $error)
+                        - {{$error }} <br>
+                        @endforeach
+                      </div>
+                  @endif
+                  <form action="{{ route('users.strore') }}" method="POST">
+                    <div class="form-row">
+                      <div class="col-sm-3">
+                        <input type="text" name="name" class="form-control" placeholder="Nombre" value="{{old('name')}}">
+                      </div>
+                      <div class="col-sm-4">
+                        <input type="text" name="email" class="form-control" placeholder="Email" value="{{old('email')}}">
+                      </div>
+                      <div class="col-sm-3">
+                        <input type="password" name="password" class="form-control" placeholder="Contraseña">
+                      </div>
+                      <div class="col-auto">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <button type="submit" class="btn btn-primary">Enviar</button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+              <div class="card border-0 shadow">
+                <div class="card-body">
+                  <table class="table">
+                    <thead>
                       <tr>
-                        <td>{{$user->id}}</td>
-                        <td>{{$user->name}}</td>
-                        <td>{{$user->email}}</td>
-                        <td>
-                          <form action="{{ route('users.destroy', $user) }}" method="POST">
-                            <input type="hidden" name="_method" value="DELETE">
-                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                            <input 
-                              type="submit" 
-                              value="Eliminar" 
-                              class="btn btn-sm btn-danger"
-                              onclick="return confirm('¿Desea eliminar...?')">
-                          </form>
-                        </td>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Email</th>
+                        <th>&nbsp;</th>
                       </tr>
-                      @if (Session::has('message'))
-                        <div class="alert alert-info">{{ Session::get('message') }}</div>
-                      @endif
-                  @endforeach
-                </tbody>
-              </table>
+                    </thead>
+                    <tbody>
+                      @foreach ($users as $user)
+                          <tr>
+                            <td>{{$user->id}}</td>
+                            <td>{{$user->name}}</td>
+                            <td>{{$user->email}}</td>
+                            <td>
+                              <form action="{{ route('users.destroy', $user) }}" method="POST">
+                                <input type="hidden" name="_method" value="DELETE">
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                <input 
+                                  type="submit" 
+                                  value="Eliminar" 
+                                  class="btn btn-sm btn-danger"
+                                  onclick="return confirm('¿Desea eliminar...?')">
+                              </form>
+                            </td>
+                          </tr>
+                          @if (Session::has('message'))
+                            <div class="alert alert-info">{{ Session::get('message') }}</div>
+                          @endif
+                      @endforeach
+                    </tbody>
+                  </table>
+                </div>
+              </div>
             </div>
           </div>
         </div>
